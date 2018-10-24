7b31acf153c82cb5b4f4cd9669f9650c69e223f5
Nima Azbijari <nazbijar@ucsd.edu>
2018-10-23 17:16:28 -1000

    Added word embedding function to use for now

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -1,5 +1,8 @@
 import lyricsgenius as lg
 import string
+import keras
+import word2vec
+from keras.preprocessing.text import Tokenizer
 
 key = "h7teesaCKXvcUvK8yxFIN1cSBSGchQ6i0MVlwE8YORNByy7N5U19geJqnKXxjCa1"
 api = lg.Genius(key)
@@ -10,8 +13,6 @@ def clean_lyrics(doc):
         doc = doc.replace(']', '')
         doc = doc.replace('(', '')
         doc = doc.replace(')', '')
-        doc = doc.replace('!', '.')
-        doc = doc.replace('?', '.')
         doc = doc.replace('Verse', '')
         doc = doc.replace('Chorus', '')
         doc = doc.replace('Outro', '')
@@ -20,22 +21,30 @@ def clean_lyrics(doc):
         doc = doc.replace('Interlude', '')
     return doc
 
-def search_by_artists(list_of_rappers, filename):
+def convert(lyrics):
+    file = open(lyrics, 'r')
+    text = file.read()
+    doc = text.split('\n')
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(doc)
+    sequences = tokenizer.texts_to_sequences(doc)
+    print(tokenizer.word_index)
 
+def search_by_artists(list_of_rappers, filename):
     with open(list_of_rappers) as f:
         file_content = f.readlines()
-
     file_content = [name.strip() for name in file_content]
-
     file = open(filename, 'w')
     for rapper in file_content:
-        artist = api.search_artist(rapper, max_songs=20, get_full_song_info=False)
+        artist = api.search_artist(rapper, max_songs=1, get_full_song_info=False)
         for s in range(len(artist.songs)):
             doc = artist.songs[s].lyrics
+            doc = clean_lyrics(doc)
             doc = doc.split('\n')
             for word in doc:
                 file.write(word+'\n')
     file.close()
 
 
-search_by_artists('rappers.txt', 'out.txt')
+search_by_artists('test.txt', 'out.txt')
+convert('out.txt')
