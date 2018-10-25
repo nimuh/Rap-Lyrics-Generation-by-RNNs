ec9137fd3a431a305273e2474e4e8d512908163d
Nima Azbijari <nazbijar@ucsd.edu>
2018-10-24 14:25:29 -1000

    Fixed indexing issue for sequence data split.

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -2,7 +2,10 @@ import lyricsgenius as lg
 import string
 import keras
 import word2vec
+import numpy as np
 from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+
 
 key = "h7teesaCKXvcUvK8yxFIN1cSBSGchQ6i0MVlwE8YORNByy7N5U19geJqnKXxjCa1"
 api = lg.Genius(key)
@@ -27,8 +30,22 @@ def convert(lyrics):
     doc = text.split('\n')
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(doc)
-    sequences = tokenizer.texts_to_sequences(doc)
-    print(tokenizer.word_index)
+    ## numpy array of list objects???
+    ## indexing last element is giving an issue ------------------|
+    sequences = tokenizer.texts_to_sequences(doc) # <---|
+    sequences = np.array(sequences)
+    input_data = []
+    targets = []
+    for seq in range(len(sequences)):
+        # this is an issue, no index is provided for new line symbol
+        if len(sequences[seq]) == 0: continue
+        input_data.append(sequences[seq][:-1])
+        targets.append(sequences[seq][len(sequences[seq])-1])
+    class_size = len(tokenizer.word_index)+1
+
+    input_data = np.array(input_data)
+    targets = np.array(targets)
+    return input_data, targets, class_size
 
 def search_by_artists(list_of_rappers, filename):
     with open(list_of_rappers) as f:
@@ -47,4 +64,8 @@ def search_by_artists(list_of_rappers, filename):
 
 
 search_by_artists('test.txt', 'out.txt')
-convert('out.txt')
+X, y, class_size = convert('out.txt')
+print("INPUTS: ")
+print(np.array(X))
+print("TARGETS: ")
+print(np.array(y))
